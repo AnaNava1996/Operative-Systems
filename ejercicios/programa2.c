b85a08116220e22a5426d6f93a7f5c376ad7ad67
Ana Paola Nava Vivas <ananava1996@gmail.com>
2017-08-19 14:15:55 -0500

    Add files via upload

---
+++ ejercicios/programa2.c
@@ -0,0 +1,20 @@
+#include stdio.h
+int main(){
+char *ptr,*ptr2;
+char cadena[100]",palabra[100];
+printf("\nIntroduzca la cadena\n");
+scanf("%[^\n]",cadena);
+printf("ingrese la palabraa eliminar:\n");
+scanf("%[^\n]",palabra);
+
+ptr=cadena;
+for(i=0;i<=100;i++){
+ptr+=1;
+if(*ptr=)
+printf("%c",*ptr);
+}
+
+
+return 0;
+}
+
